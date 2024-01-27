e9795e5e09854bdef32635e3717bd37e71302ead
ravwanovv05 <samidilloravshanov14@gmail.com>
2024-01-27 15:08:06 +0500

    change

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from api.views.dish import CreateDishGenericAPIView
+from api.views.saved_recipe import SavedRecipeGenericAPIView, SavedRecipeDetailGenericAPIView
 
 urlpatterns = [
     path('create-dish', CreateDishGenericAPIView.as_view(), name='create-dish'),
+    path('saved-recipe', SavedRecipeGenericAPIView.as_view(), name='saved-recipe'),
+    path('saved-recipe-detail/<int:pk>', SavedRecipeDetailGenericAPIView.as_view(), name='saved-recipe-detail'),
+
 ]
