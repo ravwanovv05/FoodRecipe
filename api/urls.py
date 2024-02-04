1cfeea803e8eb68e43e98e9ba8094e7945e8a3f6
ravwanovv05 <samidilloravshanov14@gmail.com>
2024-02-04 14:30:36 +0500

    added CountSavedRecipeAPIView

--- api/urls.py
+++ api/urls.py
@@ -3,13 +3,14 @@ from api.views.category import CategoryGenericAPIView
 from api.views.comment_recipe import CommentRecipeGenericAPIView, CommentsRecipeGenericAPIView
 from api.views.dish import CreateDishGenericAPIView, MyRecipeGenericAPIView, MyRecipeDetailGenericAPIView
 from api.views.rate_to_recipe import RateToRecipeGenericAPIView
-from api.views.saved_recipe import SavedRecipeGenericAPIView, SavedRecipeDetailGenericAPIView, UnSaveRecipeAPIView
+from api.views.saved_recipe import SavedRecipeGenericAPIView, SavedRecipeDetailGenericAPIView, UnSaveRecipeAPIView, CountSavedRecipeAPIView
 from api.views.search_recipe import SearchRecipeListAPIView
 
 urlpatterns = [
     path('create-dish', CreateDishGenericAPIView.as_view(), name='create-dish'),
     path('saved-recipe', SavedRecipeGenericAPIView.as_view(), name='saved-recipe'),
     path('saved-recipe-detail/<int:dish_id>', SavedRecipeDetailGenericAPIView.as_view(), name='saved-recipe-detail'),
+    path('count-saved-recipe/<int:dish_id>', CountSavedRecipeAPIView.as_view(), name='count-saved-recipe'),
     path('unsave-recipe/<int:dish_id>', UnSaveRecipeAPIView.as_view(), name='unsave-recipe'),
     path('rateto-recipe', RateToRecipeGenericAPIView.as_view(), name='rateto-recipe'),
     path('category', CategoryGenericAPIView.as_view(), name='category'),
