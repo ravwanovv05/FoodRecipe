beed423fa3023159b68a86fe1b36f6e7a18f6163
ravwanovv05 <samidilloravshanov14@gmail.com>
2024-02-05 15:32:07 +0500

    change

--- api/views/rate_to_recipe.py
+++ api/views/rate_to_recipe.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+
+from api.models.recipe_rate import RecipeRate
 from api.serializers.recipe_rate_serializer import RecipeRateSerializer
 
 
@@ -9,6 +11,9 @@ class RateToRecipeGenericAPIView(GenericAPIView):
     serializer_class = RecipeRateSerializer
 
     def post(self, request):
+        if RecipeRate.objects.filter(user_id=request.user.id, dish_id=request.data['dish_id']).exists():
+            return Response(status=400)
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
