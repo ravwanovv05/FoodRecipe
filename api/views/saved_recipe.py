c20a5a26ef6c9d1c9331c7caff7adba058a5aa74
ravwanovv05 <samidilloravshanov14@gmail.com>
2024-02-07 11:00:02 +0500

    change

--- api/views/saved_recipe.py
+++ api/views/saved_recipe.py
@@ -2,7 +2,6 @@ from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-
 from accounts.models.user_follow import UserFollow
 from api.models.dish import Dish
 from api.models.notification_saved_recipe import NotificationSaveRecipe
