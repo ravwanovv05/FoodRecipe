c009541026f7efa4df19e4833901d0db38bd23a4
ravwanovv05 <samidilloravshanov14@gmail.com>
2024-02-14 09:51:40 +0500

    change

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'config.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'config.settings'
+settings_module = 'config.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
